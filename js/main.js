c0c6d90f372f17487884f0cd9f31af7bbcfbbee3
Va1yusha <vbugreeva06@gmail.com>
2025-03-11 18:19:22 +0700

    Fourth commit

--- js/main.js
+++ js/main.js
@@ -9,5 +9,16 @@ let app = new Vue({
         altText: "A pair of socks",
         inStock: true,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
     }
 })
